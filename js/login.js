78f40d212addab0c20c22447804b4dd0d0ddadc2
Archibaldo8 <leandrovegadure@gmail.com>
2022-08-20 18:28:01 -0300

    ejercicios bien
    deberia andar casi todo

--- js/login.js
+++ js/login.js
@@ -7,18 +7,31 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
-function loginValidation(fields) {
-for field in fieldList 
-
-  if (!(field.value == "") {
+function loginValidation(correo, pass) {
+  if (!(correo.value == "") && !(pass.value == "")) {
     return alert("bienvenido rey"), redirect();
   } else {
-    //   return (correo.className += " is-invalid");
-    return alert("no es por ahi crack");
+    return (
+      alert("no es por ahi crack"),
+      (correo.className += " is-invalid"),
+      (pass.className += " is-invalid")
+    );
   }
 }
 
 // function loginWithGoogle() {
+function handleCredentialResponse(response) {
+  // decodeJwtResponse() is a custom function defined by you
+  // to decode the credential response.
+  const responsePayload = decodeJwtResponse(response.credential);
+
+  console.log("ID: " + responsePayload.sub);
+  console.log("Full Name: " + responsePayload.name);
+  console.log("Given Name: " + responsePayload.given_name);
+  console.log("Family Name: " + responsePayload.family_name);
+  console.log("Image URL: " + responsePayload.picture);
+  console.log("Email: " + responsePayload.email);
+}
 
 // }
 
